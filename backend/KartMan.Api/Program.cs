d7747f10c36b66f4fa09d3330e9f908db3b2c1fe
Ivan Zyranau <ewancoder@gmail.com>
2024-06-06 02:44:13 +0400

    Add cors & api endpoints

--- backend/KartMan.Api/Program.cs
+++ backend/KartMan.Api/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using KartMan.Api;
 using KartMan.Host;
 using Microsoft.AspNetCore.Builder;
@@ -12,6 +13,15 @@ builder.Services.AddHttpClient();
 builder.Services.AddSingleton<HistoryDataRepository>();
 builder.Services.AddHostedService<HistoryDataCollectorService>();
 
+builder.Services.AddCors(x =>
+{
+    x.AddPolicy("Cors", x => x
+        .WithOrigins("http://localhost:4200", "https://kartman.typingrealm.com", "https://dev.kartman.typingrealm.com")
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials());
+});
+
 var app = builder.Build();
 var weatherStore = app.Services.GetRequiredService<IWeatherStore>();
 
@@ -24,6 +34,56 @@ app.MapGet("/api/weather/{date}", async (DateTime date) =>
 {
     return await weatherStore.GetWeatherDataForAsync(date.Date);
 });
+
+var repository = app.Services.GetRequiredService<HistoryDataRepository>();
+
+app.MapGet("/api/sessions/{session}", async (string session) =>
+{
+    return await repository.GetSessionInfoAsync(session);
+});
+
+app.MapGet("/api/history/today", async () =>
+{
+    var history = await repository.GetHistoryForDayAsync(DateOnly.FromDateTime(DateTime.UtcNow));
+
+    return history
+        .GroupBy(x => x.session)
+        .OrderByDescending(g => g.Max(x => x.recordedAtUtc))
+        .Select(x => x
+            .OrderBy(y => y.kart)
+            .ThenBy(y => y.lap))
+        .SelectMany(x => x)
+        .ToList();
+});
+
+app.MapGet("/api/history/{session}/{kart}", async (int session, string kart) =>
+{
+    var history = (await repository.GetHistoryForDayAsync(DateOnly.FromDateTime(DateTime.UtcNow)))
+        .Where(x => x.session == session && x.kart == kart)
+        .Select(x => new ShortEntry(x.lap, x.time))
+        .ToList();
+
+    return history
+        .OrderBy(x => x.lap)
+        .ToList();
+});
+
+app.MapGet("/api/history/{dateString}", async (string dateString) =>
+{
+    var date = DateTime.ParseExact(dateString, "dd-MM-yyyy", null);
+    var history = await repository.GetHistoryForDayAsync(DateOnly.FromDateTime(date));
+
+    return history
+        .GroupBy(x => x.session)
+        .OrderByDescending(g => g.Max(x => x.recordedAtUtc))
+        .Select(x => x
+            .OrderBy(y => y.kart)
+            .ThenBy(y => y.lap))
+        .SelectMany(x => x)
+        .ToList();
+});
+
+app.UseCors("Cors");
 
 app.Services.GetRequiredService<WeatherGatherer>();
 await app.RunAsync();
