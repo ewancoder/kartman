9cdb0e6b75b2a897437101f622b095dcd4b1f1ee
Ivan Zyranau <ewancoder@gmail.com>
2024-09-13 17:30:17 +0400

    Add new Angular frontend

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'kman-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AppComponent {
+}
