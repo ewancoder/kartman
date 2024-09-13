2129c2f7cf4620ce86d5f9cfdb28591f8afdd605
Ivan Zyranau <ewancoder@gmail.com>
2024-09-13 22:06:31 +0400

    Load sessions one by one

--- frontend/src/app/sessions/session.component.ts
+++ frontend/src/app/sessions/session.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, signal, WritableSignal } from '@angular/core';
 import { SessionInfo } from './session.service';
 import { AsyncPipe, NgClass } from '@angular/common';
 import { SessionDataComponent } from '../session-data/session-data.component';
@@ -15,15 +15,35 @@ import { SessionInfoComponent } from '../session-info/session-info.component';
 export class SessionComponent {
     @Input({required: true}) session!: SessionInfo;
     @Input() lazy: boolean = false;
+    @Input() deferLoadByMs: number = 0;
     hidden: boolean = false;
+    shouldLoad: WritableSignal<boolean> = signal(false);
 
     constructor(private cdr: ChangeDetectorRef) {}
+
+    ngOnInit() {
+      if (!this.lazy) {
+        this.load();
+      }
+    }
 
     toggleHideOrLoad(): void {
       if (!this.lazy) {
         this.hidden = !this.hidden;
       } else {
+        this.load();
         this.lazy = false;
+      }
+    }
+
+    private load(): void {
+      if (!this.deferLoadByMs) {
+        this.shouldLoad.set(true);
+      } else {
+        setTimeout(() => {
+          this.shouldLoad.set(true);
+          console.log('should show up');
+        }, this.deferLoadByMs);
       }
     }
 }
