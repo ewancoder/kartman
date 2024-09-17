9aa8404e79ee652d1e2246b37be49c4bae57a75d
Ivan Zyranau <ewancoder@gmail.com>
2024-09-18 00:31:44 +0400

    Properly get fastest lap so that auto updating sessions works

--- frontend/src/app/stint-laps/stint-laps.component.ts
+++ frontend/src/app/stint-laps/stint-laps.component.ts
@@ -1,5 +1,5 @@
 import { NgClass } from '@angular/common';
-import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { LapEntry } from '../sessions/session.service';
 import { StintLapComponent } from '../stint-lap/stint-lap.component';
 
@@ -10,18 +10,10 @@ import { StintLapComponent } from '../stint-lap/stint-lap.component';
     templateUrl: './stint-laps.component.html',
     styleUrl: './stint-laps.component.scss'
 })
-export class StintLapsComponent implements OnInit, OnChanges {
+export class StintLapsComponent {
     @Input({ required: true }) laps!: LapEntry[];
-    fastestLapTime!: number;
 
-    ngOnInit() {
-        this.fastestLapTime = Math.min(...this.laps.filter(lap => !lap.isInvalidLap).map(lap => lap.lapTime));
-    }
-
-    ngOnChanges() {
-        // TODO: Figure out how to do this without ngOnChanges hook.
-        // This happens X = KARTS times per LATEST session.
-        console.log('changes');
-        this.fastestLapTime = Math.min(...this.laps.filter(lap => !lap.isInvalidLap).map(lap => lap.lapTime));
+    getFastestLap() {
+        return Math.min(...this.laps.filter(lap => !lap.isInvalidLap).map(lap => lap.lapTime));
     }
 }
