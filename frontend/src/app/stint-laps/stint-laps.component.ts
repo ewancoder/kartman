09bc071c62ff674dacd402b9118e6e99db87e06e
Ivan Zyranau <ewancoder@gmail.com>
2024-09-17 23:40:39 +0400

    Re-calculate fastest laptime on each change (BAD)

--- frontend/src/app/stint-laps/stint-laps.component.ts
+++ frontend/src/app/stint-laps/stint-laps.component.ts
@@ -1,7 +1,7 @@
 import { NgClass } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
-import { StintLapComponent } from '../stint-lap/stint-lap.component';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { LapEntry } from '../sessions/session.service';
+import { StintLapComponent } from '../stint-lap/stint-lap.component';
 
 @Component({
     selector: 'kman-stint-laps',
@@ -10,11 +10,18 @@ import { LapEntry } from '../sessions/session.service';
     templateUrl: './stint-laps.component.html',
     styleUrl: './stint-laps.component.scss'
 })
-export class StintLapsComponent implements OnInit {
+export class StintLapsComponent implements OnInit, OnChanges {
     @Input({ required: true }) laps!: LapEntry[];
     fastestLapTime!: number;
 
     ngOnInit() {
-        this.fastestLapTime = Math.min(...this.laps.map(lap => lap.lapTime));
+        this.fastestLapTime = Math.min(...this.laps.filter(lap => !lap.isInvalidLap).map(lap => lap.lapTime));
+    }
+
+    ngOnChanges() {
+        // TODO: Figure out how to do this without ngOnChanges hook.
+        // This happens X = KARTS times per LATEST session.
+        console.log('changes');
+        this.fastestLapTime = Math.min(...this.laps.filter(lap => !lap.isInvalidLap).map(lap => lap.lapTime));
     }
 }
