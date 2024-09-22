ad7f9943fd7f8e3e883d49795c456bfe7b686731
Ivan Zyranau <ewancoder@gmail.com>
2024-09-22 17:47:05 +0400

    Add a very simple unit test to Kartman API

---
+++ backend/KartMan.Api.Tests/Framework/AutoMoqDataAttribute.cs
@@ -0,0 +1,27 @@
+﻿namespace KartMan.Api.Tests;
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+public sealed class InlineAutoMoqDataAttribute : InlineAutoDataAttribute
+{
+    public InlineAutoMoqDataAttribute(params object[] objects)
+        : base(new AutoMoqDataAttribute(), objects) { }
+}
+
+[AttributeUsage(AttributeTargets.Method)]
+public sealed class AutoMoqDataAttribute : AutoDataAttribute
+{
+    public AutoMoqDataAttribute() : base(CreateFixture)
+    {
+    }
+
+    public static IFixture CreateFixture()
+    {
+        var fixture = new Fixture()
+            .Customize(new AutoMoqCustomization { ConfigureMembers = true });
+
+        fixture.Register<Stream>(() => new MemoryStream());
+        fixture.Register(() => new MemoryStream());
+
+        return fixture;
+    }
+}
---
+++ backend/KartMan.Api.Tests/Weather/WeatherRetrieverTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Net;
+using KartMan.Api.Weather;
+using Microsoft.Extensions.Configuration;
+using Moq.Protected;
+
+namespace KartMan.Api.Tests.Weather;
+
+public class WeatherRetrieverTests : Testing<WeatherRetriever>
+{
+    [Theory, AutoMoqData]
+    public async Task GetWeatherAsync_ShouldGetWeather()
+    {
+        var httpClientFactory = Freeze<IHttpClientFactory>();
+
+        var handler = new Mock<HttpMessageHandler>();
+
+        using var client = new HttpClient(handler.Object);
+        httpClientFactory.Setup(x => x.CreateClient(string.Empty))
+            .Returns(client);
+
+        handler.Protected().Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+            .Returns(async (HttpRequestMessage request, CancellationToken token) =>
+            {
+                var response = new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent("""
+                        {
+                            "current": {
+                                "temp_c": 25,
+                                "condition": {
+                                    "code": 1000
+                                }
+                            }
+                        }
+                    """)
+                };
+
+                return response;
+            });
+
+        var config = Freeze<IConfiguration>();
+        config.Setup(x => x["WeatherApiKey"])
+            .Returns("key");
+
+        var sut = Fixture.Create<WeatherRetriever>();
+
+        var weather = await sut.GetWeatherAsync();
+
+        Assert.NotNull(weather);
+        Assert.Equal(25, weather.TempC);
+        Assert.Equal(1000, weather.ConditionCode);
+    }
+}
